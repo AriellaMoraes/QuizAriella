24e1d1f597afa5c612d5d2a0cd7fe0e28391956a
Ariella Moraes <ariellamoraes@hotmail.com>
2022-11-17 20:56:01 -0300

    Envio das questoes e contagem de acertos

---
+++ database/perguntas.py
@@ -0,0 +1,53 @@
+perguntas = [
+    {
+        'id': 1,
+        'pergunta': '1 - Qual a minha idade?',
+        'respostas': [['36', False], ['30', False], ['33', True]]
+    },
+    {
+        'id': 2,
+        'pergunta': '2 - Qual o meu signo?',
+        'respostas': [['Libra', False], ['Capricornio', True], ['sagitario', False]]
+    },
+    {
+        'id': 3,
+        'pergunta': '3 - Qual o dia e mes do meu aniversario?',
+        'respostas': [['11 de janeiro', True], ['20 de janeiro', False ], ['11 de dezembro', False]]
+    },
+    {
+        'id': 4,
+        'pergunta': '4 - Para mim, onde seriam as ferias perfeitas?',
+        'respostas': [['Praia', True], ['Montanha', False ], ['Lugares Historicos', False]]
+    },
+    {
+        'id': 5,
+        'pergunta': '5 - Qual a minha estacao do ano favorita?',
+        'respostas': [['Verao', True ], ['Inverno', False ], ['Primavera', False ]]
+    },
+    {
+        'id': 6,
+        'pergunta': '6 - Qual o numero do meu calcado?',
+        'respostas': [['37', False ], ['38', True ], ['35', False ]]
+    },
+    {
+        'id': 7,
+        'pergunta': '7 - Qual o meu prato predileto?',
+        'respostas': [['Macarronada', False ], ['Feijoada', False ], ['Lasanha', True]]
+    },
+    {
+        'id': 8,
+        'pergunta': '8 - Qaundo estou triste, qual alimento eo meu kit da felicidade?',
+        'respostas': [['Coxinha e coca-cola', True ], ['Brigadeiro', False], ['Pizza', False]]
+    },
+    {
+        'id': 9,
+        'pergunta': '9 - Quais sao minhas duas cores favoritas?',
+        'respostas': [['Rosa e Vermelho', False ], ['Rosa e Roxo', True ], ['Amarelo e Laranja', False]]
+    },
+    {
+        'id': 10,
+        'pergunta': '10 - O que nessa vida e mais importante para mim?',
+        'respostas': [['Dinheiro', False ], ['Sucesso', False ], ['Amor', True]]
+    },
+
+]
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 # !/usr/bin/env python
 # coding: utf-8
 from flask import Flask, render_template, request, url_for
+from database.perguntas import perguntas
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+@app.route('/nome', methods=['GET', 'POST'])
+def nome():
+    data = request.form.to_dict() 
+    app.logger.info(data)
+    return render_template('nome.html', nome=data)
+
+def calcula(resultado):
+    cont = 0
+    for resposta in resultado.values():
+        if resposta == "True":
+            cont +=1
+    return cont
+
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    data = request.form.to_dict() 
+    cont = calcula(data)
+    app.logger.info(data)
+    return render_template('formulario.html', perguntas = perguntas, cont = cont)
+
 
 app.run(port=5000, host='localhost', debug=True)
