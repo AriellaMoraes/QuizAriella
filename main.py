1b8e30367ba5f8ce64c7fd53f9db12be018cc7ed
Ariella Moraes <ariellamoraes@hotmail.com>
2022-11-18 16:29:21 -0300

    Adicionando sessão

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 # !/usr/bin/env python
 # coding: utf-8
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
+from flask_session import Session
 from database.perguntas import perguntas
 
 app = Flask(__name__, template_folder='templates')
 
+SESSION_TYPE = 'filesystem'
+app.config["SESSION_PERMANENT"] = False
+app.config.from_object(__name__)
+Session(app)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+   
 
 @app.route('/nome', methods=['GET', 'POST'])
 def nome():
     data = request.form.to_dict() 
+    session['nome'] = data.get('nome') 
+    session['temp'] = 'Ariella'
     app.logger.info(data)
-    return render_template('nome.html', nome=data)
+    return render_template('nome.html', data=data)
 
 def calcula(resultado):
     cont = 0
@@ -24,10 +33,11 @@ def calcula(resultado):
 
 @app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
+    nome = session['nome']
     data = request.form.to_dict() 
     cont = calcula(data)
     app.logger.info(data)
-    return render_template('formulario.html', perguntas = perguntas, cont = cont)
+    return render_template('formulario.html', perguntas = perguntas, cont = cont, nome=nome)
 
 
 app.run(port=5000, host='localhost', debug=True)
